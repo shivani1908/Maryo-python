ad28b85353d7abc67a23f7927f67ae3018178113
Shivani Agrawal <shivagra1908@gmail.com>
2017-04-24 01:26:58 +0530

    first commit

---
+++ finalproject.py
@@ -0,0 +1,264 @@
+import pygame,random,sys
+from pygame.locals import *
+pygame.init()
+
+black = (0,0,0)
+blue = (0,0,255)
+white = (255,255,255)
+
+fps = 25
+window_width = 1200
+window_height = 600
+level = 0
+addnewflamerate = 25
+
+class dragon:
+    global firerect, Canvas, imagerect
+    up = False
+    down = True
+    velocity = 15
+
+    def __init__(self):
+        self.image = load_image("dragon.png")
+        self.imagerect = self.image.get_rect()
+        self.imagerect.right = window_width
+        self.imagerect.top = window_height/2
+
+
+    def update(self):
+        if(self.imagerect.top < cactusrect.bottom):
+            self.up = False
+            self.down = True
+        if(self.imagerect.bottom > firerect.top):
+            self.up = True
+            self.down = False
+        if(self.down):
+            self.imagerect.bottom += self.velocity
+        if(self.up):
+            self.imagerect.top -= self.velocity
+
+        Canvas.blit(self.image, self.imagerect)
+
+    def return_height(self):
+        h = self.imagerect.top
+        return h
+
+class flames:
+    flamespeed = 20
+
+    def __init__(self):
+        self.image = load_image("fireball.png")
+        self.imagerect = self.image.get_rect()
+        self.height = Dragon.return_height() + 20
+        self.surface = pygame.transform.scale(self.image,(20,20))
+        self.imagerect = pygame.Rect(window_width - 100, self.height, 20, 20)
+
+    def update(self):
+        self.imagerect.left -= self.flamespeed
+
+    def collision(self):
+        if(self.imagerect.left == 0):
+            return True
+        else:
+            return False
+
+class maryo:
+    global moveup,movedown,cactusrect,firerect,gravity
+    speed = 10
+    downspeed = 20
+
+    def __init__(self):
+        self.image = load_image("maryo.png")
+        self.imagerect = self.image.get_rect()
+        self.imagerect.left = 50
+        self.imagerect.top = window_height/2
+        self.score = 0
+
+    def update(self):
+        if(moveup and (self.imagerect.top > cactusrect.bottom)):
+            self.imagerect.top -= self.speed
+            self.score += 1
+            
+        if(movedown and (self.imagerect.bottom < firerect.top)):
+            self.imagerect.bottom += self.downspeed
+            self.score += 1
+
+        if (gravity and (self.imagerect.bottom < firerect.top)):  #no key is pressed so score is not changed
+            self.imagerect.bottom += self.speed
+
+def terminate(): #end the game
+    pygame.quit()
+    sys.exit()
+
+def waitforkey(): #wait for key to be pressed
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return
+
+def flamehitsmario(playerrect,flames): #to check if flame has hit mario or not
+    for f in flame_list:
+        if(playerrect.colliderect(f.imagerect)):
+           return True
+        else:
+           return False
+def drawtext(text,font,surface,x,y): #to display text on screen 
+           textobj = font.render(text,1,white)
+           textrect = textobj.get_rect()
+           textrect.topleft = (x,y)
+           surface.blit(textobj, textrect)
+
+def check_level(score):  #to check the level
+           global window_height,cactusrect,firerect,level
+
+           if score in range(0,250):
+               level = 1
+               firerect.top = window_height - 50
+               cactusrect.bottom = 50
+           elif score in range(250,500):
+               level = 2
+               firerect.top = window_height - 100
+               cactusrect.bottom = 100
+           elif score in range(500,750):
+               level = 3
+               firerect.top = window_height - 150
+               cactusrect.bottom = 150
+           elif score in range(750,1000):
+               level = 4
+               firerect.top = window_height - 200
+               cactusrect.bottom = 200
+
+def load_image(imagename):
+            image = pygame.image.load(imagename)
+            return image
+
+
+mainClock = pygame.time.Clock()
+Canvas = pygame.display.set_mode((window_width,window_height))
+caption = pygame.display.set_caption("MARYO")
+
+font = pygame.font.SysFont(None,48)
+scorefont = pygame.font.SysFont(None,45)
+
+fireimage = load_image("fire_bricks.png")
+firerect = fireimage.get_rect()
+
+cactusimage = load_image("cactus_bricks.png")
+cactusrect = cactusimage.get_rect()
+
+startimage = load_image("start.png")
+startimagerect = startimage.get_rect()
+startimagerect.centerx = window_width/2
+startimagerect.centery = window_height/2
+           
+endimage = load_image("end.png")
+endimagerect = endimage.get_rect()
+endimagerect.centerx = window_width/2
+endimagerect.centery = window_height/2
+
+pygame.mixer.music.load("mario_theme.wav")
+gameover = pygame.mixer.Sound("mario_dies.wav")
+
+#getting to the start screen
+drawtext("Mario",font,Canvas,(window_width/3),(window_height/3))
+Canvas.blit(startimage,startimagerect)
+pygame.display.update()
+
+#initialize the game
+waitforkey()
+topscore = 0
+Dragon = dragon()
+
+while True:
+    flame_list = [] #create a flame list
+    player = maryo()
+    moveup = movedown = gravity = False
+    flameaddcounter = 0
+
+    gameover.stop()
+    pygame.mixer.music.play(-1,0.0) #play the music
+
+    while True: #begin game loop
+
+        for event in pygame.event.get(): #event handling
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    moveup = True
+                    movedown = False
+                    gravity = False
+                if event.key == K_DOWN:
+                    moveup = False
+                    movedown = True
+                    gravity = False
+
+            if event.type == KEYUP:
+                if event.key == K_UP:
+                    moveup = False
+                    gravity = True
+                if event.key == K_DOWN:
+                    movedown = False
+                    gravity = True
+                if event.key == K_ESCAPE:
+                    terminate()
+
+                    
+
+        flameaddcounter += 1
+        check_level(player.score)
+
+        if flameaddcounter == addnewflamerate:
+
+            flameaddcounter = 0
+            newflame = flames()
+            flame_list.append(newflame)
+
+        for f in flame_list:
+            flames.update(f)
+
+        for f in flame_list:
+            if f.imagerect.left <= 0:
+                flame_list.remove(f)
+
+        player.update()
+        Dragon.update()
+
+        Canvas.fill(black) #fill up the screen
+        Canvas.blit(fireimage,firerect)
+        Canvas.blit(cactusimage,cactusrect)
+        Canvas.blit(player.image,player.imagerect)
+        Canvas.blit(Dragon.image,Dragon.imagerect)
+
+        drawtext("Score : %s | Top Score : %s | Level : %s"%(player.score,topscore,level),scorefont,Canvas,300,cactusrect.bottom + 10)
+
+        for f in flame_list:
+            Canvas.blit(f.image,f.imagerect)
+
+        if flamehitsmario(player.imagerect,flame_list):
+            if player.score > topscore:
+                topscore = player.score
+            break
+
+        if(player.imagerect.top <= cactusrect.bottom or player.imagerect.bottom >= firerect.top):
+            if player.score > topscore:
+                topscore = player.score
+            break
+
+        pygame.display.update()
+
+        mainClock.tick(fps)
+
+    #play game over music here
+    pygame.mixer.music.stop()
+    gameover.play()
+    Canvas.blit(endimage,endimagerect)
+    pygame.display.update()
+    waitforkey()
+
+
